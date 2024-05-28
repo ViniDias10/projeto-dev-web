203c3f7fc1f6c30c1631ef33492944960272114b
Vinicius Dias <134970909+ViniDias10@users.noreply.github.com>
2024-05-28 18:10:55 -0300

    Upload 28.05
    Página de contato pronta

--- Projeto Desenvolvimento Web/JS/script contato.js
+++ Projeto Desenvolvimento Web/JS/script contato.js
@@ -14,3 +14,12 @@ function scrollFunction() {
         document.getElementById("btnTopo").style.display = "none";
     }
 }
+
+//Funcionalidade mensagem de cofirmação
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Impede o comportamento padrão do formulário
+
+    // Mostrar a mensagem de confirmação
+    document.getElementById('confirmationMessage').style.display = 'block';
+  });
