d36bb74c10e4aa9d2ffae4d964e9427b5b4ef3e9
Vinicius Dias <134970909+ViniDias10@users.noreply.github.com>
2024-06-06 22:00:41 -0300

    Upload 06/06

--- Projeto Desenvolvimento Web/JS/script ingresso.js
+++ Projeto Desenvolvimento Web/JS/script ingresso.js
@@ -1,16 +1,3 @@
-// Funcionalidade seta
 
-function topFunction() {
-    document.body.scrollTop = 0; 
-    document.documentElement.scrollTop = 0; 
-}
 
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("btnTopo").style.display = "block";
-    } else {
-        document.getElementById("btnTopo").style.display = "none";
-    }
-}
